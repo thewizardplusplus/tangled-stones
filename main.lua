da2ad7599d19ad0200570aeee10cc00d0e661c06
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-04 23:00:00 +0300

    Fix paths to libraries

--- main.lua
+++ main.lua
@@ -1,3 +1,8 @@
+package.path = "/sdcard/lovegame/vendor/?.lua;"
+  .. "/sdcard/lovegame/vendor/?/init.lua;"
+  .. "./vendor/?.lua;"
+  .. "./vendor/?/init.lua"
+
 function love.draw()
   love.graphics.print("Hello, world!", 100, 100)
 end
