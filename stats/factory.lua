a2447f27f1b98659dde14852e0ea934d69f9c5ce
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-12 23:00:00 +0300

    Add the factory module

---
+++ stats/factory.lua
@@ -0,0 +1,15 @@
+local StatsStorage = require("stats.statsstorage")
+
+local factory = {}
+
+function factory.create_stats_storage(path, initial_minimal)
+  local ok = love.filesystem.createDirectory(path)
+  if not ok then
+    return nil, "unable to create the stats DB"
+  end
+
+  local full_path = love.filesystem.getSaveDirectory() .. "/" .. path
+  return StatsStorage:new(full_path, initial_minimal)
+end
+
+return factory
