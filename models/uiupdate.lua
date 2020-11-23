ac306fb0c2ef4cc89fa01e7898c5305e679c3e8e
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-23 23:00:00 +0300

    Add asserts to the UiUpdate class

--- models/uiupdate.lua
+++ models/uiupdate.lua
@@ -3,6 +3,8 @@ local middleclass = require("middleclass")
 local UiUpdate = middleclass("UiUpdate")
 
 function UiUpdate:initialize(reset)
+  assert(type(reset) == "boolean")
+
   self.reset = reset
 end
 
