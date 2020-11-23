e96e7a7f9b815dba3cccd502217c899257dfe79c
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-23 23:00:00 +0300

    Add asserts to the Stats class

--- models/stats.lua
+++ models/stats.lua
@@ -1,8 +1,12 @@
 local middleclass = require("middleclass")
+local typeutils = require("typeutils")
 
 local Stats = middleclass("Stats")
 
 function Stats:initialize(current, minimal)
+  assert(typeutils.is_number_with_limits(current, 0))
+  assert(typeutils.is_number_with_limits(minimal, 0))
+
   self.current = current
   self.minimal = minimal
 end
