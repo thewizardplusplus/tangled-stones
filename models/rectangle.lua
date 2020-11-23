69b8b307b67e485212fe2c1840fb4da42d6b47e3
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-23 23:00:00 +0300

    Add asserts to the Rectangle class

--- models/rectangle.lua
+++ models/rectangle.lua
@@ -1,8 +1,14 @@
 local middleclass = require("middleclass")
+local typeutils = require("typeutils")
 
 local Rectangle = middleclass("Rectangle")
 
 function Rectangle:initialize(x, y, width, height)
+  assert(typeutils.is_number_with_limits(x, 0))
+  assert(typeutils.is_number_with_limits(y, 0))
+  assert(typeutils.is_number_with_limits(width, 0))
+  assert(typeutils.is_number_with_limits(height, 0))
+
   self.x = x
   self.y = y
   self.width = width
