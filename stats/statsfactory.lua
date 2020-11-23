af0c8a12ee2f5471c3f2e41e9e165c2f38db45fa
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-23 23:00:00 +0300

    Add asserts to the statsfactory module

--- stats/statsfactory.lua
+++ stats/statsfactory.lua
@@ -1,8 +1,12 @@
 local StatsStorage = require("stats.statsstorage")
+local typeutils = require("typeutils")
 
 local statsfactory = {}
 
 function statsfactory.create_stats_storage(path, initial_minimal)
+  assert(type(path) == "string")
+  assert(typeutils.is_number_with_limits(initial_minimal, 0))
+
   local ok = love.filesystem.createDirectory(path)
   if not ok then
     return nil, "unable to create the stats DB"
