a2f06042200fb7820c1f4945117bf966fc0b0c68
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-25 23:00:00 +0300

    Add docs for the Rectangle class

--- models/rectangle.lua
+++ models/rectangle.lua
@@ -1,8 +1,25 @@
+---
+-- @classmod Rectangle
+
 local middleclass = require("middleclass")
 local typeutils = require("typeutils")
 
+---
+-- @table instance
+-- @tfield number x [0, ∞)
+-- @tfield number y [0, ∞)
+-- @tfield number width [0, ∞)
+-- @tfield number height [0, ∞)
+
 local Rectangle = middleclass("Rectangle")
 
+---
+-- @function new
+-- @tparam number x [0, ∞)
+-- @tparam number y [0, ∞)
+-- @tparam number width [0, ∞)
+-- @tparam number height [0, ∞)
+-- @treturn Rectangle
 function Rectangle:initialize(x, y, width, height)
   assert(typeutils.is_positive_number(x))
   assert(typeutils.is_positive_number(y))
