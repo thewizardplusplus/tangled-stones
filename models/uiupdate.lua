dd0698fa74eb8d534544c332ab01b2d4681cd115
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-28 23:00:00 +0300

    Add docs for the UiUpdate class

--- models/uiupdate.lua
+++ models/uiupdate.lua
@@ -1,7 +1,18 @@
+---
+-- @classmod UiUpdate
+
 local middleclass = require("middleclass")
+
+---
+-- @table instance
+-- @tfield bool reset
 
 local UiUpdate = middleclass("UiUpdate")
 
+---
+-- @function new
+-- @tparam bool reset
+-- @treturn UiUpdate
 function UiUpdate:initialize(reset)
   assert(type(reset) == "boolean")
 
