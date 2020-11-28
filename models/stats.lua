c30c421b9fb58ea77bb594f4b6f8db4719ada900
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-28 23:00:00 +0300

    Add docs for the Stats class

--- models/stats.lua
+++ models/stats.lua
@@ -1,8 +1,21 @@
+---
+-- @classmod Stats
+
 local middleclass = require("middleclass")
 local typeutils = require("typeutils")
 
+---
+-- @table instance
+-- @tfield number current [0, ∞)
+-- @tfield number minimal [0, ∞)
+
 local Stats = middleclass("Stats")
 
+---
+-- @function new
+-- @tparam number current [0, ∞)
+-- @tparam number minimal [0, ∞)
+-- @treturn Stats
 function Stats:initialize(current, minimal)
   assert(typeutils.is_positive_number(current))
   assert(typeutils.is_positive_number(minimal))
