5921d34dcad2e02ae31e50a0b47b2ded5651eed5
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-28 23:00:00 +0300

    Add docs for the statsfactory module

--- stats/statsfactory.lua
+++ stats/statsfactory.lua
@@ -1,8 +1,16 @@
+---
+-- @module statsfactory
+
 local StatsStorage = require("stats.statsstorage")
 local typeutils = require("typeutils")
 
 local statsfactory = {}
 
+---
+-- @tparam string path
+-- @tparam number initial_minimal [0, ∞)
+-- @treturn StatsStorage
+-- @error error message
 function statsfactory.create_stats_storage(path, initial_minimal)
   assert(type(path) == "string")
   assert(typeutils.is_positive_number(initial_minimal))
