c4874c2055e58f147b62a3c639481956388af7c3
thewizardplusplus <thewizardplusplus@yandex.ru>
2026-07-26 11:48:25 +0500

    Use the Font Awesome font for icons

---
+++ constants/icons.lua
@@ -0,0 +1,14 @@
+---
+-- @module icons
+
+local utf8 = require("utf8")
+
+local icons = {}
+
+---
+-- @table icons
+-- @tfield string RESET_ICON
+
+icons.RESET_ICON = utf8.char(0xf01e)
+
+return icons
